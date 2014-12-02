aed670dae5f1d128f201f1e54d0aeec076bc5949
Joshua Westgard <jwestgard@mac.com>
2014-12-02 18:43:04 -0500

    add control.py

---
+++ tests/validate_tests.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from nose.tools import *
+import validate
+
+def setup():
+	print("SETUP!")
+	
+def teardown():
+	print("TEAR DOWN!")
+	
+def test_basic():
+	print("I RAN!")
+	
