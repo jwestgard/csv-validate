0e5b69a785d55ebe97559b6012eca41c310dcdf2
Joshua Westgard <jwestgard@mac.com>
2015-02-01 12:21:00 -0500

    add control character checking

--- requirements.md
+++ requirements.md
@@ -1,1 +1,7 @@
-Individual requirements will be created as issues, and then sythesized here into a single list.
+# Requirements
+
+  1. In order to make effective use of csvkit, it is necessary to check first for the presence of control characters in the input data.  This check should be performed first in the following steps:
+    a. read the file line by line;
+    b. strip out the trailing newline (which is an necessary control char in CSV);
+    c. check for each diallowed control char in the line;
+    d. if found, report back the character by line and location index.
--- validate/control.py
+++ validate/control.py
@@ -1,11 +1,24 @@
 #!/usr/bin/env python3
 
-import sys, csvkit, curses.ascii
+import sys
 
-# inputfile = sys.argv[1]
+inputfile = sys.argv[1]
 
 controlchars = [b'\x00', b'\x01', b'\x02', b'\x03', b'\x04', b'\x05', b'\x06', b'\x07', b'\x08', b'\x09', b'\x0a', b'\x0b', b'\x1c', b'\x1d', b'\x1e', b'\x1f', b'\x10', b'\x11', b'\x12', b'\x13', b'\x14', b'\x15', b'\x16', b'\x17', b'\x18', b'\x19', b'\x1a', b'\x1b', b'\x1c', b'\x1d', b'\x1e', b'\x1f']
 
-for c in controlchars:
-	print(hex(c))
-	
+def charcheck(mystring, chars):
+    for c in chars:
+        if c in mystring:
+            charlocation = mystring.find(c)
+            print("Found {0} at {1}".format(c, charlocation))
+
+with open(inputfile, 'r') as f:
+    for n, line in enumerate(f):
+        print("\nLine {0}".format(n))
+        print("========")
+        line = line.rstrip('\n')
+        print("String -> {0}".format(line))
+        bytes = str.encode(line)
+        print("Bytes -> {0}".format(bytes))
+        charcheck(bytes, controlchars)
+
