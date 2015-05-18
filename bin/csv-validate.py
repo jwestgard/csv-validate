242e95fae5394345a3cf21dae3e0a7167c750038
Joshua Westgard <jwestgard@mac.com>
2015-05-18 18:19:22 -0400

    testing for populated cols and value lists

--- bin/csv-validate.py
+++ bin/csv-validate.py
@@ -1,43 +1,112 @@
 #!/usr/bin/env python3
 
-import csv
-import subprocess
-import sys
-import yaml
+import csv, pickle, subprocess, sys, yaml
 
 datafile = sys.argv[1]
 schemafile = sys.argv[2]
+controlchars = pickle.load(open('../lib/control.p', 'rb'))
 
+#= Function ===========
+# Load spreadsheet data
+#======================
 def read_spreadsheet(datafile):
+    print('\nLoading spreadsheet data from {0}...'.format(datafile))
     if datafile.endswith(('.xlsx','.xls')):
-        cmd = ["in2csv",datafile]
+        cmd = ['in2csv',datafile]
         rawdata = subprocess.check_output(cmd)
-        decoded = rawdata.decode('utf-8').split("\n")
-        data = [row for row in csv.DictReader(decoded)]
+        reader = csv.DictReader(rawdata.decode('utf-8').split('\n'))
     else:
         with open(datafile, 'r') as f:
-            data = [row for row in csv.DictReader(f)]    
-    return data
+            reader = csv.DictReader(f)
+    fields = reader.fieldnames
+    data = [row for row in reader]
+    return data, fields
 
+#= Function ===========
+# Read schema file
+#======================
 def read_schema(schemafile):
+    print('\nLoading schema from {0}...'.format(schemafile))
     with open(schemafile, 'r') as f:
         schema = yaml.load(f)
         return schema
 
-data = read_spreadsheet(datafile)
+#= Function ===================
+# Check for ASCII control chars
+#==============================        
+def charcheck(mystring, chars):
+    for c in chars:
+        if c in mystring:
+            charlocation = mystring.find(c)
+            print('Found {0} at {1}'.format(c, charlocation))
+
+#======================
+# Main logic
+#======================
+
+## CHECK CONTROL CHARS -- NEED TO DO BEFORE DICTREADER AS CHARS WILL MESS UP PARSING
+## 
+# with open(inputfile, 'r') as f:
+#     for n, line in enumerate(f):
+#         print("\nLine {0}".format(n))
+#         print("========")
+#         line = line.rstrip('\n')
+#         print("String -> {0}".format(line))
+#         bytes = str.encode(line)
+#         print("Bytes -> {0}".format(bytes))
+#         charcheck(bytes, [x[0] for x in controlchars])
+
+print('\nLoaded set of ASCII control characters to check.')
+print(['{0} : {1}'.format(c[0], c[3]) for c in controlchars])
+
+data, fields = read_spreadsheet(datafile)
+print('{0} lines of spreadsheet data loaded.'.format(len(data)))
+
 schema = read_schema(schemafile)
-required_cols = [x['name'] for x in schema if 'required' in x.items()]
-populated_cols = [x['name'] for x in schema if 'populated' in x.items()]
+print(schema)
 
-print("Required Columns: {0}".format(', '.join(required_cols)))
-print("Populated Columns: {0}".format(', '.join(populated_cols)))
+# check for required columns
+required = [x['name'] for x in schema if 'required' in x and x['required'] == True]
+print('\nRequired Columns: {0}.'.format(', '.join(required)))
+print('\nChecking for required columns...')
+for c in required:
+    if c in fields:
+        print('Required column {0} found.'.format(c))
+    else:
+        print('--> ERROR: Required column {0} not found!'.format(c))
+        
+# create lists of columns that must be populated  
+populated = [x['name'] for x in schema if 'populated' in x and x['populated'] == True]
+print('\nPopulated Columns: {0}.'.format(', '.join(populated)))
 
-# columns_to_check = sorted(firstline.split(','))
-# required_columns =[ item['name'] for item in mydata]
-# 
-# for c in required_columns:
-#     if c in columns_to_check:
-#         print("Found {0}!".format(c))
-#     else:
-#         print("--> ERROR: {0} not found!".format(c))
-#     
+# create restricted value lists 
+value_restricted_cols = {x['name'] : x['values'] for x in schema if 'values' in x}
+print('Value restrictions: {0}'.format(value_restricted_cols))
+
+
+for num, row in enumerate(data):
+    print('\nRow {0}:'.format(num))
+    # check populated columns
+    for pcol in populated:
+        if pcol not in row:
+            print('--> ERROR: Column {0} must exist AND cannot be empty!'.format(pcol))
+        elif row[pcol] == '':
+            print('--> ERROR: Column {0} cannot be empty!'.format(pcol))
+        else:
+            print('Column {0} correctly populated in row {1}.'.format(pcol, num))
+    # check column value restrictions
+    for vcol in value_restricted_cols:
+        if vcol not in row:
+            print('--> ERROR: Column {0} must exist AND be in {1}!'.format(vcol,
+                value_restricted_cols[vcol]))
+        elif row[vcol] not in value_restricted_cols[vcol]:
+            print('--> ERROR: Value {0} not valid for column {1}!'.format(row[vcol],
+                vcol))
+        else:
+            print('Column {0} correctly populated in row {1}.'.format(vcol, num))
+        
+
+# generate report
+#   missing columns
+#   missing values by row
+#   bad values in columns
--- docs/requirements.md
+++ docs/requirements.md
@@ -1,6 +1,32 @@
 # Requirements
 
-  1. In order to make effective use of csvkit, it is necessary to check first for the presence of control characters in the input data.  This check should be performed first in the following steps:
+  1. The ultimate goal is to allow for the validation of CSV data according to a flexible set of rules defined in a schema file.
+  2. The tool must deal elegantly with non-printing control characters that often creep into CSV files when exported from Excel.
+  3. The tool should produce reports of anomalies (violations of schema) and statistics about CSV data.
+  4. The tool should allow the user to make batch corrections to data, and should facilitate the correction of anomalies in the data.
+  5. The tool should allow for local correction and validation of CSV data before import into other systems.
+
+
+## Use Case: Audio/Video Batch Loader
+
+The xml generator script needs to perform additional validation of the metadata before ingest in order to ensure that the ingested audio objects will be compatible with the digital collections admin interface.
+
+Currently, it is possible to create XML files that omit required elements, and Fedora will allow these items to exist, but when such an item is opened in the admin interface, omitted items can lead to unexpected results, such as defaulting to 21st century the covTime element.
+
+Items that should be validated include:
+
+   - dates in ISO format (YYYY, YYYY-MM, or YYYY-MM-DD)
+   - century must be populated in form YYYY-YYYY
+   - mediatype must be populated
+   - browse terms must be present for all AlbUM items
+   - XMLType fields must be populated (for all rows to be ingested), and if multi-rowed metadata layout, each UMDM must have at least one UMAM
+
+The script should enable validation to happen separately from the ingest (pre-validation), and should create a validation report, summarizing the batch and listing each non-conformant line of data.
+
+
+## Pseudocode
+
+  1. Check first for the presence of control characters in the input data.  This check should be performed first in the following steps:
     - read the file line by line;
     - strip out the trailing newline (which is an necessary control char in CSV);
     - check for each diallowed control char in the line;
@@ -13,4 +39,3 @@
   4. Load schema file and check data against the schema. Schema file should be a yaml file for easy creation by users.
   
   5. Generate report.
-  
